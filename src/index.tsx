a71a189babb47c3cdfa000f921d6476bf535ce74
Jefferson472 <jefferson472@hotmail.com>
2022-09-01 15:43:42 -0300

    build: add styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
+import { Root } from 'root'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 )
 root.render(
   <StrictMode>
-    <div />
+    <Root />
   </StrictMode>,
 )
